de6232a941a459e1138a11b643f37a0ecb6c7dc0
harihargithub <121367488+harihargithub@users.noreply.github.com>
2023-07-06 19:51:20 +0530

    Delete ticket-tracker-app directory

--- ticket-tracker-app/src/main/java/com/gl/ttp/repository/TicketRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gl.ttp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gl.ttp.model.Ticket;
-
-public interface TicketRepository extends JpaRepository<Ticket, Long>{
-
-}
